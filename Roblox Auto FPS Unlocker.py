7345171b4776ea2f8a401920c52913794a94ed5a
afy <73753274+afyzone@users.noreply.github.com>
2023-06-28 22:27:21 -0700

    Update Roblox Auto FPS Unlocker.py

--- Roblox Auto FPS Unlocker.py
+++ Roblox Auto FPS Unlocker.py
@@ -13,20 +13,21 @@ system('title ' + 'Made by @leadmarker and @afyzone')
 for filename in os.listdir(roblox_directory):
     f = os.path.join(roblox_directory, filename)
     final = f + '\ClientSettings'
-    if not os.path.exists(final) and os.path.isdir(final):
+    if not os.path.exists(final) and os.path.isdir(f):
         os.makedirs(final)
 
-    if os.path.isdir(final) and not os.path.exists(final + '/ClientAppSettings.json'):
-        r = requests.get(url = JSON_FILE)
-        if r.status_code == 200:
-            with open(final + '/ClientAppSettings.json', 'wb') as file:
-                file.write(r.content)
+    if os.path.isdir(f):
+        if not os.path.exists(final + '/ClientAppSettings.json'):
+            r = requests.get(url = JSON_FILE)
+            if r.status_code == 200:
+                with open(final + '/ClientAppSettings.json', 'wb') as file:
+                    file.write(r.content)
 
-            print(Fore.GREEN + 'SUCCESS: Everything worked correctly, FPS unlocked for this version!')
+                print(Fore.GREEN + 'SUCCESS: Everything worked correctly, FPS unlocked for this version!')
+            else:
+                print(Fore.RED + 'ERROR: An error occured while attempting to call the URL!')
         else:
-            print(Fore.RED + 'ERROR: An error occured while attempting to call the URL!')
-    else:
-        print(Fore.BLUE + 'INFO: FPS already unlocked for this version!')
+            print(Fore.BLUE + 'INFO: FPS already unlocked for this version!')
 
 getpass.getpass(Fore.YELLOW + 'Press Enter key to exit!')
 system("exit")
